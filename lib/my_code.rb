b49d0c1a644c680ddedb308f496f68bb6fded32b
Ana Robalo <anameciarobalo@gmail.com>
2019-11-12 14:10:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -65,5 +65,13 @@ end
 
 def reduce_to_any_true(source_array)
   result =false 
-  
+    i=0 
+  while i < source_array.length do
+    if source_array[i]
+      result = true
+      return result
+    end
+    i += 1 
+  end 
+  result
 end
